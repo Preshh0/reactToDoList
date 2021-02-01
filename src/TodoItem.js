c36caf0275868c1688428dd554596eabfaaf95c7
Preshh0 <uniquep201@gmail.com>
2021-02-01 08:22:32 +0100

    New commit.

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -4,11 +4,11 @@ function TodoItem(props) {
     return (
       <div className="todo-item">
   
-          <input type = "checkbox" />
+          <input type = "checkbox" checked={props.item.completed} />
           <p> {props.item.text} </p>
 
       </div>
     );
-  }
+  } 		
   
 export default TodoItem
