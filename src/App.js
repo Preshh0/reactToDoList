69231af228a4391f80ad6cba0812a7da91866b8c
Preshh0 <uniquep201@gmail.com>
2021-02-07 22:40:02 +0100

    Used the class state function. In the App.js file, thr commented out code is my own rendering. I'm using the example given though, for consistency and incase i make a mistake i need to correct.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,62 @@
+// // import logo from './logo.svg';
+// import './App.css';
+
+// import TodoItem from "./TodoItem"
+// import todosData from "./todosData"
+// import React from 'react';
+
+// class App extends React.Component{
+
+//     constructor(){
+//         super()
+//         this.state = {
+//             todoItems: todosData.map(item => <TodoItem key= {item.id} item={item} />)
+//         }
+//     }
+
+  
+//     render(){
+//         return (
+//             <div className="todo-list">
+
+//                 {this.state.todoItems}
+
+//             </div>
+//         )
+//     }
+  
+// }
+
+// export default App;
+
 // import logo from './logo.svg';
 import './App.css';
 
 import TodoItem from "./TodoItem"
 import todosData from "./todosData"
+import React from 'react';
 
-function App() {
+class App extends React.Component{
 
-  const todoItems = todosData.map(item => <TodoItem key= {item.id} item={item} />)
+    constructor(){
+        super()
+        this.state = {
+            todos: todosData
+        }
+    }
+
   
-  return (
-    <div className="todo-list">
+    render(){
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item}/>)
+        return (
+            <div className="todo-list">
 
-        {todoItems}
+                {todoItems}
 
-    </div>
-  );
+            </div>
+        )
+    }
+  
 }
 
 export default App;
